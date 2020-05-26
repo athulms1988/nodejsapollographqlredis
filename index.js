33ce7a5ec91c46db90bdc2f8ac727d0ee11d7596
Athul M S <v-asalimkumar@carrentals.com>
2020-05-26 16:20:18 +0530

    Implementing products schema

--- index.js
+++ index.js
@@ -2,10 +2,70 @@ const { ApolloServer, gql } = require('apollo-server');
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('data.json', {encoding:'utf8', flag:'r'}));
 const typeDefs = gql`
+  type Amount {
+    total: Float
+    daily: Float
+    payLater: Float
+    payNow: Float
+    taxesAndFees: Float
+  }
+  type Price {
+    amount: Amount
+    supplierAmount: Amount
+  }
+  type PriceBreakdown {
+    amount: Float
+    supplierAmount: Float
+    quantity: Int
+    unit: String
+  }
+  type Fees {
+    amount: Float
+    supplierAmount: Float
+    description: String
+    unit: String
+  }
+  type classObjectBillingTerms {
+    text: String
+  }
+  type StationID {
+    pickUp: String
+    dropOff: String
+  }
+  type PolicyID {
+    id: ID!
+    code: String
+    description: String
+    classObjectBillingTerms: classObjectBillingTerms
+    mandatory: Boolean
+    inclusive: Boolean
+    exclusive: Boolean
+    supportedCountries: [String]
+  }
+  type Bundles {
+    id: Int
+    description: String
+    name: String
+    policyUrl: String
+
+  }
   type Product {
     productKey: ID!
     availability: String
     carId: Int
+    policyIds: [String]
+    bundledIds: [String]
+    extrasIds: [String]
+    supplierId: Int
+    stationId: StationID
+    type: String
+    rateType: String
+    creditCardRequired: String
+    price: Price
+    priceBreakdown: [PriceBreakdown]
+    fees: [Fees]
+    supplierCurrency: String
+    deeplink: String
   }
   type Query {
     products: [Product]
@@ -14,7 +74,7 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    products: () => data.products,
+    products: () => data.products
   },
 };
 
